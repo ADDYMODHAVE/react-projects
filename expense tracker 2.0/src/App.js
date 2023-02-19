f714330ac9f3f5d61ebc221d89de5767b58194b2
ADDYMODHAVE <118976941+ADDYMODHAVE@users.noreply.github.com>
2023-02-19 13:38:41 +0530

    just basic sign in and signup page added
    firebase kinked

---
+++ expense tracker 2.0/src/App.js
@@ -0,0 +1,12 @@
+import { Fragment } from "react";
+import SignUp from "./Pages/SignUp";
+
+function App() {
+  return (
+    <Fragment>
+      <SignUp />
+    </Fragment>
+  );
+}
+
+export default App;
