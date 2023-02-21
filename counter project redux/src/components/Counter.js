6b8bc7cf06bbf781a40eb27e6f5f6b5f5df6e2b9
ADDYMODHAVE <118976941+ADDYMODHAVE@users.noreply.github.com>
2023-02-21 14:35:54 +0530

    Add files via upload

--- counter project redux/src/components/Counter.js
+++ counter project redux/src/components/Counter.js
@@ -1,27 +1,37 @@
-import { useSelector,useDispatch } from 'react-redux';
+import { useSelector, useDispatch } from 'react-redux';
 
+import { counterActions } from '../store/index';
 import classes from './Counter.module.css';
 
 const Counter = () => {
-  const dispatch=useDispatch();
-  const counter = useSelector(state => state.counter);
+  const dispatch = useDispatch();
+  const counter = useSelector((state) => state.counter);
+  const show = useSelector((state) => state.showCounter);
 
-  const toggleCounterHandler = () => {};
+  const incrementHandler = () => {
+    dispatch(counterActions.increment());
+  };
 
-  const incrementHandler=()=>{
-   dispatch({type:"increment"});
-  }
-  const decrementHandler=()=>{
-    dispatch({type:"decrement"});
-   }
- 
+  const increaseHandler = () => {
+    dispatch(counterActions.increase(10)); // { type: SOME_UNIQUE_IDENTIFIER, payload: 10 }
+  };
+
+  const decrementHandler = () => {
+    dispatch(counterActions.decrement());
+  };
+
+  const toggleCounterHandler = () => {
+    dispatch(counterActions.toggleCounter());
+  };
+
   return (
     <main className={classes.counter}>
       <h1>Redux Counter</h1>
-      <div className={classes.value}>{counter}</div>
+      {show && <div className={classes.value}>{counter}</div>}
       <div>
         <button onClick={incrementHandler}>Increment</button>
-        <button onClick={decrementHandler}>Increment</button>
+        <button onClick={increaseHandler}>Increase by 10</button>
+        <button onClick={decrementHandler}>Decrement</button>
       </div>
       <button onClick={toggleCounterHandler}>Toggle Counter</button>
     </main>
