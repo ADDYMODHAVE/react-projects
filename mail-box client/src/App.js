165e287617c134d570ca4955d755af2d0437e6d1
ADDYMODHAVE <118976941+ADDYMODHAVE@users.noreply.github.com>
2023-02-25 15:09:20 +0530

    added link to header inbox added

--- mail-box client/src/App.js
+++ mail-box client/src/App.js
@@ -3,22 +3,26 @@ import "./App.css";
 import AuthPage from "./component/Pages/AuthPage";
 import { Route } from "react-router-dom";
 import Mailfirst from "./component/Mail/ComposeMail";
+import Inbox from "./component/Mail/Inbox";
 
 function App() {
   return (
     <main>
-    <h1>MailBox</h1>
-    <Route path="/" exact>
-      <AuthPage />
-    </Route>
-    <Route path="/auth" exact>
-      <AuthPage />
-    </Route>
-    <Route path="/mail">
-      <Mailfirst/>
-    </Route>
-  </main>
-);
+      <h1>MailBox</h1>
+      <Route path="/" exact>
+        <AuthPage />
+      </Route>
+      <Route path="/auth" exact>
+        <AuthPage />
+      </Route>
+      <Route path="/inbox" exact>
+        <Inbox />
+      </Route>
+      <Route path="/mail">
+        <Mailfirst />
+      </Route>
+    </main>
+  );
 }
 
 export default App;
