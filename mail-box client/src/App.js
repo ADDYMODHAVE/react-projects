acfb4dd542fbd630766079beff994afee4630977
ADDYMODHAVE <118976941+ADDYMODHAVE@users.noreply.github.com>
2023-02-25 11:56:50 +0530

    added sign up and login form
    added logic for confirm password

---
+++ mail-box client/src/App.js
@@ -0,0 +1,11 @@
+import AuthForm from "./component/AuthForm";
+
+function App() {
+  return (
+    <div className="container">
+      <AuthForm></AuthForm>
+    </div>
+  );
+}
+
+export default App;
