2afc1c12837dc343d14cdb4bfc3384ff52a555cb
ADDYMODHAVE <118976941+ADDYMODHAVE@users.noreply.github.com>
2023-02-25 12:46:30 +0530

    when user logged in show the welcome page

--- mail-box client/src/App.js
+++ mail-box client/src/App.js
@@ -1,9 +1,22 @@
-import AuthForm from "./component/AuthForm";
+import AuthForm from "./component/Login/AuthForm";
+import WelcomePage from "./component/Pages/Welcomepages";
+import { Route,Switch,Redirect } from "react-router-dom";
 
 function App() {
   return (
     <div className="container">
-      <AuthForm></AuthForm>
+        <Route path="*">
+          <Redirect to="authpage" />
+        </Route>
+     <Switch>
+     <Route path="/authpage">
+          <AuthForm/>
+        </Route>
+      
+       <Route path="/welcomepage">
+          <WelcomePage/>
+        </Route>
+      </Switch>
     </div>
   );
 }
